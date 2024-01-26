a9279044b763abbec3508b6c77f1b074184d0b82
vladimirven001 <vladimir.venkov@mail.mcgill.ca>
2024-01-26 17:27:39 -0500

    initial file structure and update to README

---
+++ SportsSchedulePlus-Backend/src/main/java/ca/mcgill/ecse321/SportsSchedulePlus/SportsSchedulePlusApplication.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.SportsSchedulePlus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsSchedulePlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsSchedulePlusApplication.class, args);
+	}
+
+}
---
+++ SportsSchedulePlus-Backend/src/test/java/ca/mcgill/ecse321/SportsSchedulePlus/SportsSchedulePlusApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.SportsSchedulePlus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsSchedulePlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
