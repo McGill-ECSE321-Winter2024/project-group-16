2de64b5d7a0abb77b73e582fda9ce3b8b492574d
dania-bh <dania.bouhmidi@mail.mcgill.ca>
2024-03-07 19:28:41 -0500

    Login, signup features work

---
+++ SportsSchedulePlus-Backend/src/main/java/ca/mcgill/ecse321/SportsSchedulePlus/security/SecurityConfig.java
@@ -0,0 +1,43 @@
+package ca.mcgill.ecse321.SportsSchedulePlus.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+  
+    @Bean
+    public static PasswordEncoder passwordEncoder(){
+    return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+    return configuration.getAuthenticationManager();
+    }
+      @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http, AuthenticationManager authenticationManager) throws Exception {
+    return http
+        .cors(AbstractHttpConfigurer::disable)
+        .csrf(AbstractHttpConfigurer::disable)
+        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//        Set permissions on endpoints
+        .authorizeHttpRequests(auth -> auth
+//            our public endpoints
+            .requestMatchers(HttpMethod.POST, "/authentication/**").permitAll()
+        
+//            our private endpoints
+            .anyRequest().authenticated())
+        .authenticationManager(authenticationManager)
+        .build();
+  }
+    
+}
