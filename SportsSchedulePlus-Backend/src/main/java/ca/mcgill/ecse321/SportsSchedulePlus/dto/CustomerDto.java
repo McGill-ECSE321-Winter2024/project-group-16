21b07b7ea04a91a4ed8dc7610de4627aeb577d0c
dania-bh <dania.bouhmidi@mail.mcgill.ca>
2024-03-07 22:09:10 -0500

    A

--- SportsSchedulePlus-Backend/src/main/java/ca/mcgill/ecse321/SportsSchedulePlus/dto/CustomerDto.java
+++ SportsSchedulePlus-Backend/src/main/java/ca/mcgill/ecse321/SportsSchedulePlus/dto/CustomerDto.java
@@ -1,61 +1,218 @@
 package ca.mcgill.ecse321.SportsSchedulePlus.dto;
-
-import java.util.List;
-import java.util.ArrayList;
+/*PLEASE DO NOT EDIT THIS CODE*/
+/*This code was generated using the UMPLE 1.33.0.6934.a386b0a58 modeling language!*/
 
 import ca.mcgill.ecse321.SportsSchedulePlus.model.Customer;
-import ca.mcgill.ecse321.SportsSchedulePlus.model.Payment;
-import ca.mcgill.ecse321.SportsSchedulePlus.model.ScheduledCourse;
 
-public class CustomerDTO extends PersonRoleDto {
+import java.util.*;
 
-    private List<PaymentDto> customerPayments;
-    private List<ScheduledCourseDTO> coursesRegistered;
+// line 26 "model.ump"
+// line 126 "model.ump"
+public class CustomerDTO extends PersonRoleResponseDTO
+{
 
-    public CustomerDTO() {
-        // Default constructor
+  //------------------------
+  // MEMBER VARIABLES
+  //------------------------
+
+  //CustomerDTO Associations
+  private List<PaymentResponseDTO> customerPayments;
+  private List<ScheduledCourseDTO> coursesRegistered;
+
+  //------------------------
+  // CONSTRUCTOR
+  //------------------------
+
+  public CustomerDTO(int aId)
+  {
+    super(aId);
+    customerPayments = new ArrayList<PaymentResponseDTO>();
+    coursesRegistered = new ArrayList<ScheduledCourseDTO>();
+  }
+
+  //------------------------
+  // INTERFACE
+  //------------------------
+  /* Code from template association_GetMany */
+  public PaymentResponseDTO getCustomerPayment(int index)
+  {
+    PaymentResponseDTO aCustomerPayment = customerPayments.get(index);
+    return aCustomerPayment;
+  }
+
+  public List<PaymentResponseDTO> getCustomerPayments()
+  {
+    List<PaymentResponseDTO> newCustomerPayments = Collections.unmodifiableList(customerPayments);
+    return newCustomerPayments;
+  }
+
+  public int numberOfCustomerPayments()
+  {
+    int number = customerPayments.size();
+    return number;
+  }
+
+  public boolean hasCustomerPayments()
+  {
+    boolean has = customerPayments.size() > 0;
+    return has;
+  }
+
+  public int indexOfCustomerPayment(PaymentResponseDTO aCustomerPayment)
+  {
+    int index = customerPayments.indexOf(aCustomerPayment);
+    return index;
+  }
+  /* Code from template association_GetMany */
+  public ScheduledCourseDTO getCoursesRegistered(int index)
+  {
+    ScheduledCourseDTO aCoursesRegistered = coursesRegistered.get(index);
+    return aCoursesRegistered;
+  }
+
+  public List<ScheduledCourseDTO> getCoursesRegistered()
+  {
+    List<ScheduledCourseDTO> newCoursesRegistered = Collections.unmodifiableList(coursesRegistered);
+    return newCoursesRegistered;
+  }
+
+  public int numberOfCoursesRegistered()
+  {
+    int number = coursesRegistered.size();
+    return number;
+  }
+
+  public boolean hasCoursesRegistered()
+  {
+    boolean has = coursesRegistered.size() > 0;
+    return has;
+  }
+
+  public int indexOfCoursesRegistered(ScheduledCourseDTO aCoursesRegistered)
+  {
+    int index = coursesRegistered.indexOf(aCoursesRegistered);
+    return index;
+  }
+  /* Code from template association_MinimumNumberOfMethod */
+  public static int minimumNumberOfCustomerPayments()
+  {
+    return 0;
+  }
+  /* Code from template association_AddUnidirectionalMany */
+  public boolean addCustomerPayment(PaymentResponseDTO aCustomerPayment)
+  {
+    boolean wasAdded = false;
+    if (customerPayments.contains(aCustomerPayment)) { return false; }
+    customerPayments.add(aCustomerPayment);
+    wasAdded = true;
+    return wasAdded;
+  }
+
+  public boolean removeCustomerPayment(PaymentResponseDTO aCustomerPayment)
+  {
+    boolean wasRemoved = false;
+    if (customerPayments.contains(aCustomerPayment))
+    {
+      customerPayments.remove(aCustomerPayment);
+      wasRemoved = true;
     }
-
-    public CustomerDTO(int id, List<PaymentDto> customerPayments, List<ScheduledCourseDTO> coursesRegistered) {
-        super(id);
-        this.customerPayments = customerPayments;
-        this.coursesRegistered = coursesRegistered;
+    return wasRemoved;
+  }
+  /* Code from template association_AddIndexControlFunctions */
+  public boolean addCustomerPaymentAt(PaymentResponseDTO aCustomerPayment, int index)
+  {  
+    boolean wasAdded = false;
+    if(addCustomerPayment(aCustomerPayment))
+    {
+      if(index < 0 ) { index = 0; }
+      if(index > numberOfCustomerPayments()) { index = numberOfCustomerPayments() - 1; }
+      customerPayments.remove(aCustomerPayment);
+      customerPayments.add(index, aCustomerPayment);
+      wasAdded = true;
     }
+    return wasAdded;
+  }
 
-       // Constructor to convert a Customer object to CustomerDto
-    public CustomerDTO(Customer customer) {
-        super(customer.getId());
-        // Assuming a constructor in PersonRoleDto that takes an ID as input
-
-        this.customerPayments = new ArrayList<>();
-        for (Payment payment : customer.getCustomerPayments()) {
-            this.customerPayments.add(new PaymentDto(payment));
-        }
-
-        this.coursesRegistered = new ArrayList<>();
-        for (ScheduledCourse scheduledCourse : customer.getCoursesRegistered()) {
-            this.coursesRegistered.add(new ScheduledCourseDTO(scheduledCourse));
-        }
+  public boolean addOrMoveCustomerPaymentAt(PaymentResponseDTO aCustomerPayment, int index)
+  {
+    boolean wasAdded = false;
+    if(customerPayments.contains(aCustomerPayment))
+    {
+      if(index < 0 ) { index = 0; }
+      if(index > numberOfCustomerPayments()) { index = numberOfCustomerPayments() - 1; }
+      customerPayments.remove(aCustomerPayment);
+      customerPayments.add(index, aCustomerPayment);
+      wasAdded = true;
+    } 
+    else 
+    {
+      wasAdded = addCustomerPaymentAt(aCustomerPayment, index);
     }
-    
+    return wasAdded;
+  }
+  /* Code from template association_MinimumNumberOfMethod */
+  public static int minimumNumberOfCoursesRegistered()
+  {
+    return 0;
+  }
+  /* Code from template association_AddUnidirectionalMany */
+  public boolean addCoursesRegistered(ScheduledCourseDTO aCoursesRegistered)
+  {
+    boolean wasAdded = false;
+    if (coursesRegistered.contains(aCoursesRegistered)) { return false; }
+    coursesRegistered.add(aCoursesRegistered);
+    wasAdded = true;
+    return wasAdded;
+  }
 
-    // Getters and setters for each attribute
-
-    public List<PaymentDto> getCustomerPayments() {
-        return customerPayments;
+  public boolean removeCoursesRegistered(ScheduledCourseDTO aCoursesRegistered)
+  {
+    boolean wasRemoved = false;
+    if (coursesRegistered.contains(aCoursesRegistered))
+    {
+      coursesRegistered.remove(aCoursesRegistered);
+      wasRemoved = true;
     }
-
-    public void setCustomerPayments(List<PaymentDto> customerPayments) {
-        this.customerPayments = customerPayments;
+    return wasRemoved;
+  }
+  /* Code from template association_AddIndexControlFunctions */
+  public boolean addCoursesRegisteredAt(ScheduledCourseDTO aCoursesRegistered, int index)
+  {  
+    boolean wasAdded = false;
+    if(addCoursesRegistered(aCoursesRegistered))
+    {
+      if(index < 0 ) { index = 0; }
+      if(index > numberOfCoursesRegistered()) { index = numberOfCoursesRegistered() - 1; }
+      coursesRegistered.remove(aCoursesRegistered);
+      coursesRegistered.add(index, aCoursesRegistered);
+      wasAdded = true;
     }
+    return wasAdded;
+  }
 
-    public List<ScheduledCourseDTO> getCoursesRegistered() {
-        return coursesRegistered;
+  public boolean addOrMoveCoursesRegisteredAt(ScheduledCourseDTO aCoursesRegistered, int index)
+  {
+    boolean wasAdded = false;
+    if(coursesRegistered.contains(aCoursesRegistered))
+    {
+      if(index < 0 ) { index = 0; }
+      if(index > numberOfCoursesRegistered()) { index = numberOfCoursesRegistered() - 1; }
+      coursesRegistered.remove(aCoursesRegistered);
+      coursesRegistered.add(index, aCoursesRegistered);
+      wasAdded = true;
+    } 
+    else 
+    {
+      wasAdded = addCoursesRegisteredAt(aCoursesRegistered, index);
     }
+    return wasAdded;
+  }
 
-    public void setCoursesRegistered(List<ScheduledCourseDTO> coursesRegistered) {
-        this.coursesRegistered = coursesRegistered;
-    }
-
+  public void delete()
+  {
+    customerPayments.clear();
+    coursesRegistered.clear();
+    super.delete();
+  }
 
 }
