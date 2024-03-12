9af556abe2b3293e1b56385419a552c34d8184e7
larrycherry13 <laurent.chiricota@gmail.com>
2024-03-11 20:47:13 -0400

    first test working customer service

--- SportsSchedulePlus-Backend/src/test/java/ca/mcgill/ecse321/SportsSchedulePlus/repository/CourseTypeRepositoryTests.java
+++ SportsSchedulePlus-Backend/src/test/java/ca/mcgill/ecse321/SportsSchedulePlus/repository/CourseTypeRepositoryTests.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
---
+++ SportsSchedulePlus-Backend/src/test/java/ca/mcgill/ecse321/SportsSchedulePlus/service/testSportsScheduleApp.java
@@ -0,0 +1,98 @@
+package ca.mcgill.ecse321.SportsSchedulePlus.service;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.when;
+import java.sql.Date;
+import java.sql.Time;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.stubbing.Answer;
+
+import ca.mcgill.ecse321.SportsSchedulePlus.model.Person;
+import ca.mcgill.ecse321.SportsSchedulePlus.repository.*;
+
+
+@ExtendWith(MockitoExtension.class)
+public class testSportsScheduleApp {
+
+    @Mock
+    private PersonRepository personRepository;
+    @Mock
+    private CourseTypeRepository courseTypeRepository;
+    @Mock
+    private CustomerRepository customerRepository;
+    @Mock
+    private DailyScheduleRepository dailyScheduleRepository;
+    @Mock
+    private InstructorRepository instructorRepository;
+    @Mock
+    private OwnerRepository ownerRepository;
+    @Mock
+    private PaymentRepository paymentRepository;
+    @Mock
+    private PersonRoleRepository personRoleRepository;
+    @Mock
+    private ScheduledCourseRepository scheduledCourseRepository;
+
+
+    @InjectMocks
+    private PersonService service;
+
+    private static final String PERSON_KEY = "TestPerson";
+
+    @BeforeEach
+    public void setMockOutput() {
+        lenient().when(personRepository.findPersonByName(anyString())).thenAnswer( (InvocationOnMock invocation) -> {
+            if(invocation.getArgument(0).equals(PERSON_KEY)) {
+                Person person = new Person();
+                person.setName(PERSON_KEY);
+                return person;
+            } else {
+                return null;
+            }
+            });
+            // Whenever anything is saved, just return the parameter object
+		Answer<?> returnParameterAsAnswer = (InvocationOnMock invocation) -> {
+			return invocation.getArgument(0);
+		};
+		lenient().when(personRepository.save(any(Person.class))).thenAnswer(returnParameterAsAnswer);
+        }
+       /*  @Test
+        public void testCreatePerson() {
+            assertEquals(0, service.getAllPersons().size());
+
+            String name = "Oscar";
+            Person person = null;
+            try {
+                person = service.createPerson(name);
+            } catch (IllegalArgumentException e) {
+                // Check that no error occurred
+                fail();
+            }
+            assertNotNull(person);
+            assertEquals(name, person.getName());*/
+	}
+
+
+    
+    
+
+
