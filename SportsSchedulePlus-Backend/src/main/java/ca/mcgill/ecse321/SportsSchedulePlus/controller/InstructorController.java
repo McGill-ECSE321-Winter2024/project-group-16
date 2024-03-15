7f243e7d978ce4102b770b4ec6b7cdb25b055558
jkzcodes <jessiekurtz123@gmail.com>
2024-03-14 21:06:34 -0400

    Removed CustomerService, DailyScheduleService, InstructorService, OwnerService, PersonService and merged all services into UserService. Will clean up to remove redundant code

--- SportsSchedulePlus-Backend/src/main/java/ca/mcgill/ecse321/SportsSchedulePlus/controller/InstructorController.java
+++ SportsSchedulePlus-Backend/src/main/java/ca/mcgill/ecse321/SportsSchedulePlus/controller/InstructorController.java
@@ -4,9 +4,8 @@ import ca.mcgill.ecse321.SportsSchedulePlus.dto.coursetype.CourseTypeRequestDTO;
 import ca.mcgill.ecse321.SportsSchedulePlus.dto.user.instructor.InstructorResponseDTO;
 import ca.mcgill.ecse321.SportsSchedulePlus.dto.user.person_person_role.PersonResponseDTO;
 import ca.mcgill.ecse321.SportsSchedulePlus.model.*;
-import ca.mcgill.ecse321.SportsSchedulePlus.service.userservice.InstructorService;
-import ca.mcgill.ecse321.SportsSchedulePlus.service.userservice.PersonService;
 import ca.mcgill.ecse321.SportsSchedulePlus.service.courseservice.ScheduledCourseService;
+import ca.mcgill.ecse321.SportsSchedulePlus.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,52 +19,51 @@ import java.util.stream.Collectors;
 public class InstructorController {
 
     @Autowired
-    private InstructorService instructorService;
+    private UserService userService;
 
     @Autowired
     private ScheduledCourseService scheduledCourseService;
 
-    @Autowired
-    private PersonService personService;
+
 
     @GetMapping(value = {"/instructors"})
     public List<PersonResponseDTO> getAllInstructors() {
-        return instructorService.getAllInstructors().stream().map(instructor -> convertToDTO(instructor)).collect(Collectors.toList());
+        return userService.getAllInstructors().stream().map(instructor -> convertToDTO(instructor)).collect(Collectors.toList());
 
     }
 
     @GetMapping(value = {"/instructors/{experience}"})
     public List<PersonResponseDTO> getInstructorByExperience(@PathVariable("experience") String experience) {
-        return instructorService.getInstructorByExperience(experience).stream().map(instructor -> convertToDTO(instructor)).collect(Collectors.toList());
+        return userService.getInstructorByExperience(experience).stream().map(instructor -> convertToDTO(instructor)).collect(Collectors.toList());
     }
 
     @GetMapping(value = {"/instructors/{email}"})
     public PersonResponseDTO getInstructor(@PathVariable("email") String email) {
-        Instructor instructor = instructorService.getInstructor(email);
+        Instructor instructor = userService.getInstructor(email);
         return convertToDTO(instructor);
     }
 
     @GetMapping(value = {"/instructors/supervised-course/{id}"})
     public List<PersonResponseDTO> getInstructorsBySupervisedCourse(@PathVariable("id") int scheduledCourseId) {
         ScheduledCourse scheduledCourse = scheduledCourseService.getScheduledCourse(scheduledCourseId);
-        return instructorService.getInstructorsBySupervisedCourse(scheduledCourse).stream().map(instructor -> convertToDTO(instructor)).collect(Collectors.toList());
+        return userService.getInstructorsBySupervisedCourse(scheduledCourse).stream().map(instructor -> convertToDTO(instructor)).collect(Collectors.toList());
     }
 
     @GetMapping(value = {"/instructors/suggested-courses"})
     public PersonResponseDTO getInstructorsBySuggestedCourse(@RequestBody CourseTypeRequestDTO courseTypeDTO) {
         CourseType courseType = new CourseType(courseTypeDTO.getDescription(), courseTypeDTO.isApprovedByOwner(), courseTypeDTO.getPrice());
-        return convertToDTO(instructorService.getInstructorBySuggestedCourseTypes(courseType));
+        return convertToDTO(userService.getInstructorBySuggestedCourseTypes(courseType));
     }
 
     @DeleteMapping(value = {"/instructors/{id}"})
     public ResponseEntity<String> deleteInstructor(@PathVariable("id") int id) {
-        int personId = instructorService.deleteInstructor(id);
+        int personId = userService.deleteInstructor(id);
         return ResponseEntity.ok("Instructor with id " + personId + " was successfully deleted.");
     }
 
     @PostMapping(value = {"/instructors/{email}"})
     public PersonResponseDTO createInstructor(@PathVariable("email") String email, @RequestBody String experience) {
-        Person person = instructorService.createInstructor(email, experience);
+        Person person = userService.createInstructor(email, experience);
         return convertToDTO(person);
     }
 
@@ -80,7 +78,7 @@ public class InstructorController {
         String updatedExperience = instructorDTO.get("experience");
 
         // Use the extracted values for the update
-        Person person = instructorService.updateInstructor(id, updatedName, updatedEmail, updatedPassword, updatedExperience);
+        Person person = userService.updateInstructor(id, updatedName, updatedEmail, updatedPassword, updatedExperience);
 
         return convertToDTO(person);
     }
@@ -88,16 +86,13 @@ public class InstructorController {
     @PostMapping(value = {"/instructors/{email}/suggest-course"})
     public ResponseEntity<String> suggestCourseType(@PathVariable("email") String email, @RequestBody CourseTypeRequestDTO courseTypeRequestDTO) {
         // Get the instructor by email
-        Instructor instructor = instructorService.getInstructor(email);
+        Instructor instructor = userService.getInstructor(email);
         if (instructor == null) {
             return ResponseEntity.notFound().build();
         }
 
-        // Create a new course type based on the request DTO
         CourseType courseType = new CourseType(courseTypeRequestDTO.getDescription(), courseTypeRequestDTO.isApprovedByOwner(), courseTypeRequestDTO.getPrice());
-
-        // Suggest the course type to the instructor
-        instructorService.suggestCourseType(instructor, courseType);
+        userService.suggestCourseType(instructor, courseType);
 
         return ResponseEntity.ok("Course type suggested successfully.");
     }
@@ -107,7 +102,7 @@ public class InstructorController {
         if (person == null) {
             throw new IllegalArgumentException("There is no such customer!");
         }
-        Instructor instructor = instructorService.getInstructor(person.getEmail());
+        Instructor instructor = userService.getInstructor(person.getEmail());
         InstructorResponseDTO instructorDTO = new InstructorResponseDTO(instructor);
         PersonResponseDTO personDTO = new PersonResponseDTO(person.getName(), person.getEmail(), person.getPassword(), instructorDTO);
         return personDTO;
@@ -118,7 +113,7 @@ public class InstructorController {
             throw new IllegalArgumentException("There is no such instructor!");
         }
         InstructorResponseDTO instructorDTO = new InstructorResponseDTO(instructor);
-        Person person = personService.getPersonById(instructor.getId());
+        Person person = userService.getPersonById(instructor.getId());
         PersonResponseDTO personDTO = new PersonResponseDTO(person.getName(), person.getEmail(), person.getPassword(), instructorDTO);
         return personDTO;
     }
