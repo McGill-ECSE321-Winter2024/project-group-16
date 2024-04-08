957b61cd4e615c63e520f7893979a800dc8a6ef8
SamySabir <samysbr@hotmail.fr>
2024-04-08 13:19:47 -0400

    updated 404

--- SportsSchedulePlus-Frontend/src/components/NotFoundPage.vue
+++ SportsSchedulePlus-Frontend/src/components/NotFoundPage.vue
@@ -1,6 +1,5 @@
 <template>
-    <div>
-        <meta charset="utf-8">
+	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
@@ -13,24 +12,26 @@
 
 	<!-- Custom stlylesheet -->
 	<link type="text/css" rel="stylesheet" href="css/style.css" />
-        <h3>Oops! Page not found.</h3>
-        <h1>
-            <span>4</span>
-            <span>0</span>
-            <span>4</span>
-        </h1>
-        <h2> we are sorry, but the page you requested was not found</h2>
-        <div class="image-container">
-            <img src="../assets/img/404.png" alt="404 Not Found" />
-        </div>
-        <div class="btn-container">
-            <ArgonButton color="primary" variant="fill" @click="goToDashboard">Go Home</ArgonButton>
-        </div>
-    </div>   
+
+	<div id="notfound">
+		<div class="notfound">
+			<div class="notfound-404">
+				<h3>Oops! Page not found</h3>
+				<h1><span>4</span><span>0</span><span>4</span></h1>
+			</div>
+			<h2>we are sorry, but the page you requested was not found</h2>
+			<div class="image-container">
+				<img src="../assets/importedpng/404.png" alt="404 Not Found" />
+			</div>
+			<div class="btn-container">
+				<ArgonButton color="primary" variant="fill" @click="goToDashboard">Go Home</ArgonButton>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script setup>
-import ArgonButton from "@/components/ArgonButton.vue";
+import ArgonButton from "@/argon_components/ArgonButton.vue";
 import { useRouter } from 'vue-router';
 
 const router = useRouter();
@@ -46,16 +47,117 @@ export default {
 </script>
 
 <style>
-    h1 {
-        font-size: 2em;
-        color: rgb(0, 0, 0);
-    }
-    p {
-        font-size: 1.5em;
-        color: white;
-    }
-    div {
-        text-align: center;
-    }
+    * {
+  -webkit-box-sizing: border-box;
+          box-sizing: border-box;
+}
+
+body {
+  padding: 0;
+  margin: 0;
+}
+
+#notfound {
+  position: relative;
+  height: 100vh;
+}
+
+#notfound .notfound {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  -webkit-transform: translate(-50%, -50%);
+      -ms-transform: translate(-50%, -50%);
+          transform: translate(-50%, -50%);
+}
+
+.notfound {
+  max-width: 520px;
+  width: 100%;
+  line-height: 1.4;
+  text-align: center;
+}
+
+.notfound .notfound-404 {
+  position: relative;
+  height: 240px;
+}
+
+.notfound .notfound-404 h1 {
+  font-family: 'Montserrat', sans-serif;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  -webkit-transform: translate(-50%, -50%);
+      -ms-transform: translate(-50%, -50%);
+          transform: translate(-50%, -50%);
+  font-size: 252px;
+  font-weight: 900;
+  margin: 0px;
+  color: #262626;
+  text-transform: uppercase;
+  letter-spacing: -40px;
+  margin-left: -20px;
+}
+
+.notfound .notfound-404 h1>span {
+  text-shadow: -8px 0px 0px #fff;
+}
+
+.notfound .notfound-404 h3 {
+  font-family: 'Cabin', sans-serif;
+  position: relative;
+  font-size: 16px;
+  font-weight: 700;
+  text-transform: uppercase;
+  color: #262626;
+  margin: 0px;
+  letter-spacing: 3px;
+  padding-left: 6px;
+}
+
+.notfound h2 {
+  font-family: 'Cabin', sans-serif;
+  font-size: 20px;
+  font-weight: 400;
+  text-transform: uppercase;
+  color: #000;
+  margin-top: 0px;
+  margin-bottom: 25px;
+}
+
+@media only screen and (max-width: 767px) {
+  .notfound .notfound-404 {
+    height: 200px;
+  }
+  .notfound .notfound-404 h1 {
+    font-size: 200px;
+  }
+}
+
+@media only screen and (max-width: 480px) {
+  .notfound .notfound-404 {
+    height: 162px;
+  }
+  .notfound .notfound-404 h1 {
+    font-size: 162px;
+    height: 150px;
+    line-height: 162px;
+  }
+  .notfound h2 {
+    font-size: 16px;
+  }
+}
+.image-container {
+    margin-bottom: -20px; /* Add some spacing between the image and button */
+    overflow: hidden; /* Hide the overflowing image */
+    text-align: center; /* Center the image horizontally */
+  }
+
+  .image-container img {
+    display: block; /* Ensure the image is treated as a block element */
+    margin: -50px auto 0; /* Center the image horizontally */
+    max-width: 400px; /* Ensure the image doesn't exceed its container's width */
+  }
     
 </style>
--- SportsSchedulePlus-Frontend/src/router/index.js
+++ SportsSchedulePlus-Frontend/src/router/index.js
@@ -1,23 +1,10 @@
 /* eslint no-undef: "off" */
 import { createRouter, createWebHistory } from "vue-router";
-<<<<<<< HEAD
-import Dashboard from "../views/Dashboard.vue";
-import Customers from "../views/Customers.vue";
-import Instructors from "../views/Instructors.vue";
 
-import Billing from "../views/Billing.vue";
-import Profile from "../views/Profile.vue";
-import Signup from "../views/Signup.vue";
-import Signin from "../views/Signin.vue";
-import Registrations from "../views/Registrations.vue";
-import CourseTypes from "../views/CourseTypes.vue";
-import ScheduledCourses from "../views/ScheduledCourses.vue";
-import NotFoundPage from "../views/NotFoundPage.vue";
-=======
+import NotFoundPage from "../components/NotFoundPage.vue";
 import Dashboard from ".././components/Dashboard.vue";
 import Customers from ".././components/Customers.vue";
 import Instructors from ".././components/Instructors.vue";
->>>>>>> 0f84a4d293f21a5f5ab29d71faaa9d8718facd51
 
 import Profile from ".././components/Profile.vue";
 import Signup from ".././components/Signup.vue";
