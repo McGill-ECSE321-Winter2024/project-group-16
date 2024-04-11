7f326212f02c2179a6477c1d5025bcaedf1bdc21
vladimirven001 <vladimirven001@gmail.com>
2024-04-11 13:53:26 -0400

    Fix #161

--- SportsSchedulePlus-Backend/src/main/java/ca/mcgill/ecse321/SportsSchedulePlus/SportsSchedulePlusApplication.java
+++ SportsSchedulePlus-Backend/src/main/java/ca/mcgill/ecse321/SportsSchedulePlus/SportsSchedulePlusApplication.java
@@ -2,8 +2,10 @@ package ca.mcgill.ecse321.SportsSchedulePlus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("ca.mcgill.ecse321.SportsSchedulePlus")
 public class SportsSchedulePlusApplication {
 
 	public static void main(String[] args) {
